b5f8fb58e965e2fdc3e120c182002f1c622d4294
KmLig <phamxuankhiem1102@gmail.com>
2022-08-22 05:07:50 +0700

    handling different routes

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/', (req, res, next) => {
+    console.log('This always runs');
+    next();
+})
+
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>"Add product" Page</h1>')
+});
+app.use('/', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Express.js</h1>')
+});
+
+app.listen(3000);

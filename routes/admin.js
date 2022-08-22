f5ae835f9533ddcb5caebcd816f972e1daa52b8b
KmLig <phamxuankhiem1102@gmail.com>
2022-08-23 04:25:21 +0700

    returning a 404 page

---
+++ routes/admin.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+const express = require('express');
+
+const router = express.Router();
+
+
+// /admin/add-product => GET request
+router.get('/add-product', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+});
+
+// /admin/add-product => POST request
+router.post('/add-product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+module.exports = router;

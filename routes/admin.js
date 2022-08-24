36aae26c3359343d65231dff4b3a1cec3a0b5e1a
KmLig <phamxuankhiem1102@gmail.com>
2022-08-25 04:36:48 +0700

    finishing the Pug template

---
+++ routes/admin.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+const products = [];
+
+// /admin/add-product => GET request
+router.get('/add-product', (req, res, next) => {
+    res.render('add-product', { pageTitle: 'Add product', path: '/admin/add-product'})
+});
+
+// /admin/add-product => POST request
+router.post('/add-product', (req, res, next) => {
+    products.push({title: req.body.title})
+    res.redirect('/');
+}) 
+
+// module.exports = router;
+exports.router = router;
+exports.products = products;

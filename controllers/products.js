520b49886f1dd537c1e4d4dfc732c58e48998fed
KmLig <phamxuankhiem1102@gmail.com>
2022-08-27 04:01:17 +0700

    adding a product model

---
+++ controllers/products.js
@@ -0,0 +1,29 @@
+const Product = require('../models/product');
+
+exports.getAddProducts = (req, res, next) => {
+  res.render("add-product", {
+    pageTitle: "Add product",
+    path: "/admin/add-product",
+    activeAddProduct: true,
+    productCSS: true,
+    formsCSS: true,
+  });
+};
+
+exports.postAddProducts = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect("/");
+};
+
+exports.getProducts = (req, res, next) => {  
+  const products = Product.fetchAll();
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
+};

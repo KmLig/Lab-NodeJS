4b56517eecff0e439a2986f32495c12f02a89e12
KmLig <phamxuankhiem1102@gmail.com>
2022-08-28 06:12:33 +0700

    rendering the product detail view

---
+++ controllers/shop.js
@@ -0,0 +1,53 @@
+const Product = require("../models/product");
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/product-list", {
+      prods: products,
+      pageTitle: "All products",
+      path: "/products"      
+    });
+  });
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findProductById(prodId, product => {
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'      
+    });
+    console.log(product);
+  })
+}
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/index", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+    });
+  });
+}
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle: "Your cart"    
+  })
+}
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: "Your orders"    
+  })
+}
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  })
+}
